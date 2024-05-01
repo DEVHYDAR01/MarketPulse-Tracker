4fb32f3ccd86d9680b59f2eb397c28285c7d1660
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-05-01 14:26:03 +0100

    This commit will push implemented search bar functionality and the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
 
 const Home = () => {
   return(
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
-        <div className="flex max-xl: flex-col gap-16">
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
               Smart Shopping Starts Here:
@@ -27,7 +28,7 @@ const Home = () => {
 
             <Searchbar/>
           </div>
-          HeroCarosel
+          <HeroCarousel/>
         </div>
       </section>
       <section className="trending-section">
