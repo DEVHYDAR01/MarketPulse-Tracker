98295e894b324f4a620e5cab118eac7fd6bd6b0a
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-05-03 18:11:50 +0100

    This commit will push database implementation

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
-import { extractCurrency, extractPrice } from '../utils';
+import { extractCurrency, extractDescription, extractPrice } from '../utils';
 
 export async function scrapeAmazonProduct(url: string) {
   if(!url) return;
@@ -53,22 +53,28 @@ export async function scrapeAmazonProduct(url: string) {
 
     const currency = extractCurrency($('.a-price-symbol'))
     const discountRate = $('.reinventPriceSavingsPercentageMargin.savingsPercentage').text().replace(/[-%]/g, "");
+
+    const description = extractDescription($)
     //construct data obeject with scraped information
     const data = {
       url,
       currency: currency || '$',
       image: imageUrls[0],
       title,
-      currentPrice: Number(currentPrice),
-      originalPrice: Number(originalPrice),
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
       priceHistory: [],
       discountRate: Number(discountRate),
       category: 'category',
       reviewsCount: 100,
       stars: 4.5,
       isOutOfStock: outOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
     }
-    console.log(data);
+    return data;
     // console.log( {title, currentPrice, originalPrice, outOfStock, imageUrls, currency, discountRate} )  
   } 
   catch (error: any) {
